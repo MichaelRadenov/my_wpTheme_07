eac70268bb110f0e2e8fc08a624122342b9b5312
Michael Radenov <radenovic@gmail.com>
2023-02-11 16:03:43 +0100

    get_template_part() added ... enqueuing css style ___ (get_stylesheet_directory_uri(glup)) ___ fixsed

--- functions.php
+++ functions.php
@@ -6,18 +6,20 @@
  * @package me_myself_&_I_Theme
  */
 
+ 
+//print_r(get_stylesheet_uri()        . '/assets/src/library/css/bootstrap.min.css');
+//wp_die();
 
 
-
-function my_enqueue_script_func () {
+function my_enqueue_script_func() {
     
     // regestring styles & scripts
-    wp_register_style ('main-css'     , get_stylesheet_uri()         . '                                         ', [        ], filemtime(get_template_directory() . '/style.css'     ), 'all');
-    wp_register_style ('bootstrap-css', get_stylesheet_uri()         . '/assets/src/library/css/bootstrap.min.css', [        ],           false                                        , 'all');
+    wp_register_style ('main-css'     , get_stylesheet_uri()                                                          , [        ], filemtime(get_template_directory() . '/style.css'     ), 'all');
+    wp_register_style ('bootstrap-css', get_stylesheet_directory_uri()   . '/assets/src/library/css/bootstrap.min.css', [        ],           false                                        , 'all');
 
-    wp_register_script('main-js'     , get_template_directory_uri() . '/assets/main.js',                            [        ], filemtime(get_template_directory() . '/assets/main.js'),  true);
-    wp_register_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.js'  , ['jquery'],           false                                        ,  true);
-
+    wp_register_script('main-js'      , get_template_directory_uri()     . '/assets/main.js'                          , [        ], filemtime(get_template_directory() . '/assets/main.js'),  true);
+    wp_register_script('bootstrap-js' , get_template_directory_uri()     . '/assets/src/library/js/bootstrap.min.js'  , ['jquery'],           false                                        ,  true);
+ 
 
     // enqueueing styles & scripts
     wp_enqueue_style('main-css'     );
@@ -29,6 +31,7 @@ function my_enqueue_script_func () {
 }
 
 add_action('wp_enqueue_scripts', 'my_enqueue_script_func');
+
 
 
 
--- index.php
+++ index.php
@@ -4,6 +4,12 @@
  * Index template file.
  *
  * @package me_myself_&_I_Theme
+ * 
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
  */
 
 get_header();
